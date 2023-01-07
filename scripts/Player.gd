798596d028c4350acb12126ccea04b1fdb56be5c
Apples <dbralir@gmail.com>
2023-01-07 12:10:01 -0600

    Yielded to navigation issues

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -20,8 +20,7 @@ var attack_available = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	anim_tree.active = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
