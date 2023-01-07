2a637a161104cc6e7a9c6d6f086e0bc0d7880aff
Jacob <jacob.wallace@utexas.edu>
2023-01-06 22:28:33 -0800

    Set up animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,7 +5,7 @@ extends KinematicBody
 # var a = 2
 # var b = "text"
 
-export(float, 0.1, 100, 0.1) var move_speed: float = 5
+export(Vector3) var move_speed = Vector3(5, 1, 5)
 export(Vector3) var gravity = Vector3(0, -10, 0)
 export(float) var resting_y_vel: float = -0.1
 
@@ -13,20 +13,33 @@ var velocity: Vector3 = Vector3(0, resting_y_vel, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Sprite.play("idle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var input = Vector3()
 	if Input.is_action_pressed("move_left"):
+		$Sprite.play("run")
+		$Sprite.flip_h = true
 		input.x -= 1
 	if Input.is_action_pressed("move_right"):
+		$Sprite.flip_h = false
+		$Sprite.play("run")
 		input.x += 1
 	if Input.is_action_pressed("move_up"):
+		$Sprite.play("run")
 		input.z -= 1
 	if Input.is_action_pressed("move_down"):
+		$Sprite.play("run")
 		input.z += 1
+		
+#	if input.x == 0 && input.z == 0:
+#		$Sprite.play("idle")
+		
+	if Input.is_action_pressed("action_attack"):
+		$Sprite.play("attack1")
+		
 	
 	var vel = input * move_speed + velocity
 	
