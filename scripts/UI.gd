ee89ee2b5a3acfe055c5006679af07af4dc9aa70
WiredOverload <mike.everywhere@gmail.com>
2023-01-06 19:31:33 -0600

    added main menu scene

---
+++ scripts/UI.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_get_materials_button_pressed():
+	#get_tree().change_scene_to_file("res://main.tscn")
+	pass # Replace with function body.
+
+
+func _on_harvest_button_pressed():
+	#get_tree().change_scene_to_file("res://main.tscn")
+	pass # Replace with function body.
+
+
+func _on_boss_battle_button_pressed():
+	#get_tree().change_scene_to_file("res://main.tscn")
+	pass # Replace with function body.
